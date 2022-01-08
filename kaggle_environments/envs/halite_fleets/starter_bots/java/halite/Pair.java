61b022b871a77859e4bafd21acdec391a1ca2586
Bovard Doerschuk-Tiberi <bovard@google.com>
2022-01-08 06:19:19 +0000

    Pair

---
+++ kaggle_environments/envs/halite_fleets/starter_bots/java/halite/Pair.java
@@ -0,0 +1,13 @@
+package halite;
+
+public class Pair<F, S> {
+
+    public final F first;
+    public final S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+}
